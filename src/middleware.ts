f10fbe035f28b0f2b1c50a87e79796a46ffe3bf8
Marcio <marcio.piva@eficazmarketing.com>
2026-02-02 17:25:03 -0300

    fix: middleware erro de cookies

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,15 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-const PUBLIC_ROUTES = ['/login', '/forgot-password', '/register', '/'];
+const PUBLIC_ROUTES = ['/', '/login', '/forgot-password', '/register'];
 const DEFAULT_PRIVATE_ROUTE = '/dashboard';
 
 export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl;
 
   const token = request.cookies.get('authToken')?.value;
-  const isPublicRoute = PUBLIC_ROUTES.some(route =>
-    pathname.startsWith(route)
-  );
+  const isPublicRoute =
+    PUBLIC_ROUTES.includes(pathname);
 
   if (token && isPublicRoute) {
     return NextResponse.redirect(
